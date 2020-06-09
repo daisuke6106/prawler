d71f61da9534d4e4ca91cc7f0007fa4b733de552
daisuke6106 <daisuke6106@gmail.com>
2020-06-10 03:39:41 +0900

    ディスクへのアウトプット処理と、ディスクからの読み込み処理を追加

--- prawler.py
+++ prawler.py
@@ -1,21 +1,121 @@
 # -*- coding: utf-8 -*-
 import requests
 import mysql.connector
+import pickle
+import hashlib
+import os
+import datetime
+
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 # ===================================================================================================
-class html_page:
+class page:
 
-    #@classmethod
-    #def read_by_mysql(datastore_mysql):
+    @staticmethod
+    def connect(url, timeout = 10):
+        req_inst = requests.get(url=url, timeout=timeout)
+        headers  = dict(req_inst.headers)
+        content  = req_inst.content
+        # content_type = req_inst.headers['content-type']
+        return page.create_page_instance(url, headers, content)
+
+    @staticmethod
+    def read_latest(url, basedir):
+        if basedir == None or basedir == "":
+            raise ValueError("basedir is set.")
+        if not os.path.isdir(basedir) :
+            raise ValueError("basedir is not dir. basedir=[{0}]".format(basedir))
+        if not basedir.endswith("/"):
+            basedir = basedir + "/"
+        urlhash = page.__url_to_hash(url)
+        basedir = basedir + urlhash + "/"
+        if not os.path.isdir(basedir) :
+            raise ValueError("specified url is not saved. url=[{0}] dir=[{1}]".format(url, basedir))
+        dir_list = page.__get_dir_list(basedir)
+        basedir = basedir + dir_list[0]
+        return page.read(url, basedir)
         
+    @staticmethod
+    def read(url, dir):
+        if dir == None or dir == "":
+            raise ValueError("dir is set.")
+        if not os.path.isdir(dir) :
+            raise ValueError("dir is not dir. dir=[{0}]".format(dir))
+        if not dir.endswith("/"):
+            dir = dir + "/"
+        with open(dir + page.__get_headers_dump_file_name() , 'rb') as f_headers:
+            headers = pickle.loads(f_headers.read())
+        with open(dir + page.__get_content_file_name() , 'rb') as f_content:
+            content = f_content.read()
+        return page.create_page_instance(url, headers, content)
+        
+    @staticmethod
+    def __url_to_hash(url):
+        return hashlib.md5(url.encode('utf-8')).hexdigest() 
 
-    def __init__(self, url, timeout=10):
+    @staticmethod
+    def __get_content_file_name():
+        return "content"
+
+    @staticmethod
+    def __get_headers_dump_file_name():
+        return "headers.dmp"
+
+    @staticmethod
+    def __get_headers_txt_file_name():
+        return "headers.txt"
+     
+    @staticmethod
+    def create_page_instance(url, header, content):
+        content_type = {v for k,v in header.items() if k.lower() == "content-type"}.pop() # filter(lambda k,v: if k.lo) header['content-type']
+        if content_type == None :
+            return page(url, header, content)
+        elif "text/html" in content_type :
+            return html_page(url, header, content)
+        else:
+            return page(url, header, content)
+
+    @staticmethod
+    def __get_dir_list(dir):
+        return list(filter(lambda dir_file: os.path.isdir(dir + dir_file), os.listdir(path=dir)))
+
+    def __init__(self, url, header, content):
         self.url = url
-        self.timeout = timeout
-        self.req = requests.get(url=self.url, timeout=self.timeout)
-        self.soup = BeautifulSoup(self.req.content, "html.parser")
+        self.headers = header
+        self.content = content
+
+
+    def save(self, basedir):
+        if basedir == None or basedir == "":
+            raise ValueError("basedir is set.")
+        if not os.path.isdir(basedir) :
+            raise ValueError("basedir is not dir. basedir=[{0}]".format(basedir))
+        if not basedir.endswith("/"):
+            basedir = basedir + "/"
+        urlhash = page.__url_to_hash(self.url)
+        basedir = basedir + urlhash + "/"
+        if not os.path.isdir(basedir) :
+            os.makedirs(basedir)
+            with open(basedir + "url", "w") as f_url:
+                f_url.write(self.url)
+        basedir = basedir + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + "/"
+        os.makedirs(basedir)
+        with open(basedir + self.__get_content_file_name(), "wb") as f_content:
+            f_content.write(self.content)
+        with open(basedir + self.__get_headers_dump_file_name(), "wb") as f_headers:
+            f_headers.write(pickle.dumps(self.headers))
+        with open(basedir + self.__get_headers_txt_file_name(), "w") as f_headers:
+            for key, value in self.headers.items():
+                f_headers.write("\"" + key +"\",\"" + value + "\"\n")
+
+
+# ===================================================================================================
+class html_page(page):
+
+    def __init__(self, url, header, content):
+        super().__init__(url = url, header = header, content = content)
+        self.soup = BeautifulSoup(self.content, "html.parser")
 
     def get_title(self):
         """
@@ -170,17 +270,23 @@ class prawler_datastore_mysql(datastore_mysql):
 
 
 
-ds = datastore_mysql()
-ds.insert("insert into USERS values(%s, %s)", ("aaa", 11))
-rows = ds.select("select * from USERS")
-for row in rows:
-    print(row[0] + str(row[1]))
-# page = html_page("http://gigazine.net")
+
+# ds = datastore_mysql()
+# ds.insert("insert into USERS values(%s, %s)", ("aaa", 11))
+# rows = ds.select("select * from USERS")
+# for row in rows:
+#     print(row[0] + str(row[1]))
+# page = page.connect("http://gigazine.net")
+# print(type(page.content))
+# print(type(page.headers))
 # element_list = page.get_element("div.content section div.card h2 span")
-# 
 # def print_content(element):
 #     print ( element.content() )
-# 
 # element_list.roop(print_content)
-# print(element_list)
+# page.save("/tmp")
 
+page_inst = page.read_latest("http://gigazine.net", "/tmp")
+element_list = page_inst.get_element("div.content section div.card h2 span")
+def print_content(element):
+    print ( element.content() )
+element_list.roop(print_content)
